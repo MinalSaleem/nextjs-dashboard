6a1c4fc42af4d6185d0e7a47ed5770839ca4ff08
MinalSaleem <minalsaleem9@gmail.com>
2024-11-25 22:55:21 +0500

    changes in page file

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,6 +7,12 @@ import { RevenueChartSkeleton,  LatestInvoicesSkeleton, CardsSkeleton, } from '@
 
 export default async function Page() {
 
+    // const {
+    //     numberOfInvoices,
+    //     numberOfCustomers,
+    //     totalPaidInvoices,
+    //     totalPendingInvoices,
+    //   } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
